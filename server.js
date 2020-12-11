718e7a0f964d0fac3643c7265d32e93de3cc9486
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-12-10 16:22:58 -0800

    routes created, delete, create

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,12 +7,89 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database.');
+});
 
-// Default response for an other request(NOt Found) catch all
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FRom candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// get a single candidate
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success!',
+            data: row
+        });
+    });
+});
+
+//Delete a candidate
+app.delete('/api/candidate/:id', (eq, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this, this.changes);
+});
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [];
+
+//ES5 function, not arrow function, to use this
+db.run(sql, params, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
+
+// Default response for an other request(NOt Found) catch all stay at the bottom
 app.use((req, res) => {
     res.status(404).end();
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB conection 
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
